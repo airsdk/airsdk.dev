fd3c510286121badf8bdff91bdb27ffbaf036279
marchbold <marchbold@users.noreply.github.com>
2021-10-15 08:41:59 +1000

    Fix for code block rendering

--- src/components/homepage/HomepageCodeBlock.tsx
+++ src/components/homepage/HomepageCodeBlock.tsx
@@ -1,36 +1,28 @@
 import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import styles from './HomepageCodeBlock.module.css';
-import Prism from 'prismjs';
-// import "../css/prism.css"
+
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import vsDark from 'prism-react-renderer/themes/vsDark';
 
 export default function HomepageCodeBlock({ id, code, language, title, description }) {
-  useEffect(() => {
-    (Prism.languages.actionscript = Prism.languages.extend('javascript', {
-      keyword:
-        /\b(?:as|break|case|catch|class|const|default|delete|do|else|extends|finally|for|function|if|implements|import|in|instanceof|interface|internal|is|native|new|null|package|private|protected|public|return|super|switch|this|throw|try|typeof|use|var|void|while|with|dynamic|each|final|get|include|namespace|native|override|set|static)\b/,
-      operator: /\+\+|--|(?:[+\-*\/%^]|&&?|\|\|?|<<?|>>?>?|[!=]=?)=?|[~?@]/,
-    })),
-      (Prism.languages.actionscript['class-name'].alias = 'function'),
-      Prism.languages.markup &&
-        Prism.languages.insertBefore('actionscript', 'string', {
-          xml: {
-            pattern:
-              /(^|[^.])<\/?\w+(?:\s+[^\s>\/=]+=("|')(?:\\[\s\S]|(?!\2)[^\\])*\2)*\s*\/?>/,
-            lookbehind: !0,
-            inside: Prism.languages.markup,
-          },
-        });
-    Prism.highlightAll();
-  }, []);
+  useEffect(() => {}, []);
   return (
     <div className={clsx('row', styles.feature, 'box')}>
       <div className={clsx('col col--7')}>
-        <div>
-          <pre>
-            <code className={`language-${language}`}>{code}</code>
-          </pre>
-        </div>
+        <Highlight {...defaultProps} theme={vsDark} code={code} language={language}>
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
       </div>
       <div className={clsx('col col--5', styles.content)}>
         <div className="text--center padding-horiz--md">
