ae674302c6b7a268445b0c4a74796ae5133851f6
gh-actions <actions@github.com>
2024-03-22 10:48:13 +0000

    Deploy website - based on 3fcd54bddc7ab07e48d019d71e8702c20f4a5c88

---
+++ assets/js/4f04b67c.2cfb3881.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkairsdk_dev=self.webpackChunkairsdk_dev||[]).push([[73599],{22042:e=>{e.exports=JSON.parse('{"permalink":"/news/page/3","page":3,"postsPerPage":10,"totalPages":4,"totalCount":31,"previousPage":"/news/page/2","nextPage":"/news/page/4","blogDescription":"AIR SDK News","blogTitle":"Blog"}')}}]);
--- assets/js/63eda9ff.40c4132b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkairsdk_dev=self.webpackChunkairsdk_dev||[]).push([[87141],{63018:e=>{e.exports=JSON.parse('{"permalink":"/news/tags/updates","page":1,"postsPerPage":10,"totalPages":3,"totalCount":28,"nextPage":"/news/tags/updates/page/2","blogDescription":"AIR SDK News","blogTitle":"Blog"}')}}]);
